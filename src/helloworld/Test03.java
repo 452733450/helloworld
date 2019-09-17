5a18fb25042882f59c2c3874d2495776ae9b0890
ypp <452733450@qq.com>
2019-09-17 11:34:46 +0800

    1

--- src/helloworld/Test03.java
+++ src/helloworld/Test03.java
@@ -3,10 +3,8 @@ package helloworld;
 public class Test03 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Test03");
-=======
 		System.out.println("3");
->>>>>>> jack
 	}
 }
